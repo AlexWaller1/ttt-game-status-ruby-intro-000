fdeafb6bc5ce3a5f4e1e42b8a8ab4d6d1c4f1339
grateful-closure-7122 <alex@artofalexwaller.com>
2020-08-23 23:47:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -41,7 +41,9 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board) || !won?(board)
+  elsif !won?(board) && !full?(board) 
+    return false
+  else won?(board)
     return false
   end
 end
